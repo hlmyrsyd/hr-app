e61fda79012ddc9869e8f95e626a4f4c191cfa95
jodyboy <elite.crew66@gmail.com>
2024-10-17 16:11:08 +0700

    update for deployment

--- app/components/wavyText/index.tsx
+++ app/components/wavyText/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { easeIn, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 export const WavyText = ({ text }: { text: string }) => {
