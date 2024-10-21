439f636e63077c0d5edf3b15891d3f78d847d81e
jodyboy <elite.crew66@gmail.com>
2024-10-21 14:46:01 +0700

    feat/add first motion in header

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,20 +1,33 @@
 'use client';
 
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+    const [initialVisible, setInitialVisible] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setInitialVisible(false);
+        }, 2000); // Make the text visible for 2 seconds
+
+        return () => clearTimeout(timer);
+    }, []);
+    
     return (
         <header className="flex w-full h-full flex-col items-center sm:items-start border-b border-black/15 dark:border-white/[.145]">
             <motion.div 
                 className="flex w-full justify-between text-wrap cursor-default font-semibold text-[16rem]"
-                whileHover="hover"          
-                style={{ fontSize: 'clamp(3rem, 12vw, 16rem)', lineHeight: '1' }}
+                whileHover="hover" 
+                whileTap="hover"     
+                style={{ fontSize: 'clamp(2rem, 11vw, 16rem)', lineHeight: '1' }}
             >
                 {/* First name */}
                 <div className="flex" title="This is my First Name">
                 <h1>H</h1>
                 <motion.h1
                     initial={{ opacity: 0 }}
+                    animate={{ opacity: initialVisible ? 1 : 0 }}
                     variants={{
                     hover: { opacity: 1, transition: { duration: 0.1, ease: "easeInOut" } }
                     }}
@@ -23,6 +36,7 @@ export default function Header() {
                 </motion.h1>
                 <motion.h1
                     initial={{ opacity: 0 }}
+                    animate={{ opacity: initialVisible ? 1 : 0 }}
                     variants={{
                     hover: { opacity: 1, transition: { duration: 0.1, delay: 0.03, ease: "easeInOut" } }
                     }}
@@ -31,6 +45,7 @@ export default function Header() {
                 </motion.h1>
                 <motion.h1
                     initial={{ opacity: 0 }}
+                    animate={{ opacity: initialVisible ? 1 : 0 }}
                     variants={{
                     hover: { opacity: 1, transition: { duration: 0.1, delay: 0.06, ease: "easeInOut" } }
                     }}
@@ -39,6 +54,7 @@ export default function Header() {
                 </motion.h1>
                 <motion.h1
                     initial={{ opacity: 0 }}
+                    animate={{ opacity: initialVisible ? 1 : 0 }}
                     variants={{
                     hover: { opacity: 1, transition: { duration: 0.1, delay: 0.09, ease: "easeInOut" } }
                     }}
@@ -51,6 +67,7 @@ export default function Header() {
                 <h1>R</h1>
                 <motion.h1
                     initial={{ opacity: 0 }}
+                    animate={{ opacity: initialVisible ? 1 : 0 }}
                     variants={{
                     hover: { opacity: 1, transition: { duration: 0.1, delay: 0.12, ease: "easeInOut" } }
                     }}
@@ -59,6 +76,7 @@ export default function Header() {
                 </motion.h1>
                 <motion.h1
                     initial={{ opacity: 0 }}
+                    animate={{ opacity: initialVisible ? 1 : 0 }}
                     variants={{
                     hover: { opacity: 1, transition: { duration: 0.1, delay: 0.15, ease: "easeInOut" } }
                     }}
@@ -67,6 +85,7 @@ export default function Header() {
                 </motion.h1>
                 <motion.h1
                     initial={{ opacity: 0 }}
+                    animate={{ opacity: initialVisible ? 1 : 0 }}
                     variants={{
                     hover: { opacity: 1, transition: { duration: 0.1, delay: 0.18, ease: "easeInOut" } }
                     }}
@@ -75,6 +94,7 @@ export default function Header() {
                 </motion.h1>
                 <motion.h1
                     initial={{ opacity: 0 }}
+                    animate={{ opacity: initialVisible ? 1 : 0 }}
                     variants={{
                     hover: { opacity: 1, transition: { duration: 0.1, delay: 0.21, ease: "easeInOut" } }
                     }}
@@ -83,6 +103,7 @@ export default function Header() {
                 </motion.h1>
                 <motion.h1
                     initial={{ opacity: 0 }}
+                    animate={{ opacity: initialVisible ? 1 : 0 }}
                     variants={{
                     hover: { opacity: 1, transition: { duration: 0.1, delay: 0.24, ease: "easeInOut" } }
                     }}
